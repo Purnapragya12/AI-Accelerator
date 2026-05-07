593f29de5b8cb6a2a98243245e974a3e5caabfef
Purnapragya12 <purnapragya12@gmail.com>
2026-05-07 22:07:22 +0530

    Fixed Streamlit imports

--- visualization/dashboard.py
+++ visualization/dashboard.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+sys.path.append(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..")
+    )
+)
+
 import streamlit as st
 import numpy as np
 import pandas as pd
